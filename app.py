8c3bd7fdbfbffca9aa741aa2726a13785338aa61
Sepevalle <sepevalle@gmail.com>
2025-07-14 00:54:59 +0200

    s

--- app.py
+++ app.py
@@ -645,6 +645,10 @@ def get_peak_elo_key(jugador):
 def index():
     datos_jugadores, timestamp = obtener_datos_jugadores()
     
+    # Si la caché está vacía (primera ejecución), podemos indicar que se está cargando.
+    # Comprobamos también el timestamp para diferenciar una lista vacía de una carga inicial.
+    cargando = not datos_jugadores and timestamp == 0
+    
     lectura_exitosa, peak_elo_dict = leer_peak_elo()
 
     if lectura_exitosa:
@@ -667,10 +671,16 @@ def index():
         for jugador in datos_jugadores:
             jugador["peak_elo"] = jugador["valor_clasificacion"] # Como fallback, mostramos el valor actual
 
-    ultima_actualizacion = (datetime.fromtimestamp(timestamp) + timedelta(hours=2)).strftime("%d/%m/%Y %H:%M:%S")
-    
-    
-    return render_template('index.html', datos_jugadores=datos_jugadores, ultima_actualizacion=ultima_actualizacion, ddragon_version=DDRAGON_VERSION)
+    if timestamp > 0:
+        ultima_actualizacion = (datetime.fromtimestamp(timestamp) + timedelta(hours=2)).strftime("%d/%m/%Y %H:%M:%S")
+    else:
+        ultima_actualizacion = "Calculando..."
+
+    return render_template('index.html', 
+                           datos_jugadores=datos_jugadores, 
+                           ultima_actualizacion=ultima_actualizacion, 
+                           ddragon_version=DDRAGON_VERSION,
+                           cargando=cargando)
 
 @app.route('/jugador/<nombre_jugador>')
 def perfil_jugador(nombre_jugador):
